6da10430c44dceb4ddcb2a2efba041610f5e7fb9
Darshit Domadiya <200540107022@darshan.ac.in>
2023-10-21 15:38:02 +0530

    first commit into github repo.

---
+++ BasicsCRUD/Controllers/ClientsController.cs
@@ -0,0 +1,132 @@
+﻿using BasicsCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BasicsCRUD.Controllers
+{
+    public class ClientsController : Controller
+    {
+        #region Private IConfiguration Variable
+
+        private IConfiguration Configuration;
+
+        #endregion
+
+        #region Constructor
+
+        public ClientsController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        #endregion
+
+        #region SelectAll (READ)
+        public IActionResult Index()
+        {
+            DataTable dt = new DataTable();
+            string str = this.Configuration.GetConnectionString("myConnectionString");
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "PR_Clients_SelectAll";
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            return View("ClientsList", dt);
+        }
+        #endregion
+
+        #region Populating form / Redirecting if want to add NEW client
+        public IActionResult Add(int ClientID)
+        {
+            if (ClientID != null)
+            {
+                string str = this.Configuration.GetConnectionString("myConnectionString");
+                SqlConnection conn = new SqlConnection(str);
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "PR_Clients_SelectByPK";
+                cmd.Parameters.Add("@ClientID", SqlDbType.Int).Value = ClientID;
+                DataTable dt = new DataTable();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                dt.Load(sdr);
+                ClientsModel modelClients = new ClientsModel();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    modelClients.ClientID = Convert.ToInt32(dr["ClientID"]);
+                    modelClients.FirstName = dr["FirstName"].ToString();
+                    modelClients.LastName = dr["LastName"].ToString();
+                    modelClients.Age = Convert.ToInt32(dr["Age"]);
+                    modelClients.Email = dr["Email"].ToString();
+                    modelClients.Gender = dr["Gender"].ToString();
+                }
+                return View("ClientsAddEdit", modelClients);
+            }
+
+            return View("ClientsAddEdit");
+        }
+        #endregion
+
+        #region Insert(Save) (CREATE / UPDATE)
+        [HttpPost]
+        public IActionResult Save(ClientsModel modelClients)
+        {
+            string str = this.Configuration.GetConnectionString("myConnectionString");
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            if (modelClients.ClientID == null)
+            {
+                cmd.CommandText = "PR_Clients_Insert";
+            }
+            else
+            {
+                cmd.CommandText = "PR_Clients_UpdateByPK";
+                cmd.Parameters.Add("@ClientID", SqlDbType.Int).Value = modelClients.ClientID;
+            }
+
+            cmd.Parameters.Add("@FirstName", SqlDbType.NVarChar).Value = modelClients.FirstName;
+            cmd.Parameters.Add("@LastName", SqlDbType.NVarChar).Value = modelClients.LastName;
+            cmd.Parameters.Add("@Age", SqlDbType.Int).Value = modelClients.Age;
+            cmd.Parameters.Add("@Email", SqlDbType.NVarChar).Value = modelClients.Email;
+            cmd.Parameters.Add("@Gender", SqlDbType.NVarChar).Value = modelClients.Gender;
+
+            if (Convert.ToBoolean(cmd.ExecuteNonQuery()))
+            {
+                if (modelClients.ClientID == null)
+                    TempData["ClientInsertMsg"] = "Client registered successfully.";
+                else
+                    return RedirectToAction("Index");
+            }
+
+            conn.Close();
+            return RedirectToAction("Index");
+
+        }
+        #endregion
+
+        #region Delete (DELETE)
+        public IActionResult Delete(int ClientID)
+        {
+            DataTable dt = new DataTable();
+            string str = this.Configuration.GetConnectionString("myConnectionString");
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "PR_Clients_Delete";
+            cmd.Parameters.AddWithValue("@ClientID", ClientID);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            return RedirectToAction("Index");
+        }
+        #endregion
+    }
+}
+
---
+++ BasicsCRUD/Models/ClientsModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BasicsCRUD.Models
+{
+    public class ClientsModel
+    {
+        public int? ClientID { get; set; }
+
+        [Required(ErrorMessage = "Firstname is Required")]
+        [DisplayName("First Name")]
+        [StringLength(maximumLength: 80, MinimumLength = 1)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Lastname is Required")]
+        [DisplayName("Last name")]
+        [StringLength(maximumLength: 80, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Age is Required")]
+        [DisplayName("Age")]
+        [Range(1, 120, ErrorMessage = "Age must be between 1 and 120.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [DisplayName("Email")]
+        [StringLength(maximumLength: 100, MinimumLength = 1)]
+        public string Email { get; set; }
+
+        [DisplayName("Gender")]
+        [StringLength(maximumLength: 30, MinimumLength = 1)]
+        public string? Gender { get; set; }
+    }
+}
